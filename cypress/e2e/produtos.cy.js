24cb86c1f46b4b51e1039f63abba49044df02ee0
Nayane Rocha <nayarocha@protonmail.com>
2026-02-10 16:48:39 -0300

    chore: organiza estrutura de pastas e corrige links
    Ajuste da pasta docs para a raiz e sincronização de caminhos de imagens para correta renderização no GitHub que permaneceram quebrados após a att 9275dd4.

--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -1,7 +1,10 @@
 /// <reference types="cypress" />
 const { faker } = require('@faker-js/faker');
 
+
 describe('Funcionalidade Cadastro', () => {
+
+    const produto01 = 'Ariel Roll Sleeve Sweatshirt'
 
     beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
@@ -17,18 +20,18 @@ describe('Funcionalidade Cadastro', () => {
             //.first()
             //.last()
             //.eq(3)
-            .contains('Ariel Roll Sleeve Sweatshirt')
+            .contains(produto01)
             .click()
     });
 
     it('Deve adicionar um produto ao carrinho', () => {
 
         cy.get('[class="product-block grid"]')
-            .contains('Ariel Roll Sleeve Sweatshirt').click()
+            .contains(produto01).click()
         cy.get('[class="variable-item button-variable-item button-variable-item-S"]').click()
         cy.get('.button-variable-item-Purple').click()
         cy.get('.input-text').clear().type(6)
         cy.get('.single_add_to_cart_button').click()
-        cy.get('.woocommerce-message').should('contain', '“Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
+        cy.get('.woocommerce-message').should('contain', `“${produto01}” foram adicionados no seu carrinho.`)
     });
 });
